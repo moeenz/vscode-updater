47d941a9133f6c0f047a1e232d8be4238a99c75f
Moeen Zamani <moeenzdev@gmail.com>
2017-02-03 00:15:56 +0330

    initiated repository and created script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Visual Studio Code Updater
+This is a simple script to automate downloading, installing and restarting vscode.
+
+## Usage
+```
+    chmod +x update.sh
+    ./update.sh
+```
+
+You can export path to this script for simpler future usages.
+
+# Note
+This script will kill all vscode processes before installing latest version, so you're responsible for saving your work before running this script.
---
+++ update.sh
@@ -0,0 +1,38 @@
+OS=$(lsb_release -si);
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+DIST="";
+
+if [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ]; then
+    DIST="deb";
+    echo "Debian based";
+elif [ "$OS" = "Fedora" ] || [ "$OS" = "Red Hat" ] || [ "$OS" = "Red hat" ]; then
+    DIST="rpm";
+    echo "Red hat based";
+else
+    echo "Unfortunately your operating system is not supported in distributed packages.";
+    exit;
+fi
+
+URLBASE="https://vscode-update.azurewebsites.net/latest/linux-${DIST}-x64/stable";
+FILENAME="$DIR/latest.${DIST}";
+
+echo "Downloading latest version of vscode is starting...";
+rm $FILENAME;
+wget $URLBASE -o $FILENAME;
+printf "Downloading finished.\n\n";
+
+echo "Closing vscode...";
+for pid in $(pidof code); do kill -9 $pid; done
+echo "vscode instance(s) closed.";
+
+echo "Installing latest version...";
+if [ "$DIST" = "deb" ]; then
+    sudo dpkg -i $FILENAME;
+else
+    sudo rpm -i $FILENAME;
+fi
+echo "Installation finished.";
+
+echo "Starting new version of vscode...";
+code &;
+exit;
