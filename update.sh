25204fc46f1d70b2472fd4526c7ef0e0ce13f5be
DrNakiz <parsahosseini2001@gmail.com>
2021-04-26 12:45:14 +0430

    removed bugs from wget error handling

--- update.sh
+++ update.sh
@@ -23,9 +23,10 @@ fi
 
 echo "Downloading latest version of vscode is starting...";
 wget --show-progress -O $FILENAME $URLBASE;
-if [ $? -ne 0 ]; then
-    echo "Failed to download."
-    exit 1
+RESULT="$?";
+if ((RESULT != 0)); then
+    echo "Failed to download.";
+    exit 1;
 fi
 printf "Downloading finished.\n\n";
 
